69eb36ad6c440a3a04f5f74be89291247ede9f26
Athul G <157095814+athulgopan016@users.noreply.github.com>
2024-03-15 13:06:08 +0530

    Delete Formproject directory

--- Formproject/formapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class MyForm(forms.Form):
-    name=forms.CharField(max_length=20)
-    address=forms.CharField(max_length=50)
-    age=forms.IntegerField()
-    phoneno=forms.CharField(max_length=10)
