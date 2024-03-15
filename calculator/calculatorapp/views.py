e5537b27db0671bd0db15caf884bc17e25aa9f9d
Athul G <157095814+athulgopan016@users.noreply.github.com>
2024-03-15 13:08:52 +0530

    Delete calculator directory

--- calculator/calculatorapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return render(request,'calculator.html')
-
-def calculation(request):
-    num1=request.POST[num1]
-
