f6bdfbb5e4016aa9df4a22ee13fa58074e57bb68
Athul G <157095814+athulgopan016@users.noreply.github.com>
2024-03-15 13:08:07 +0530

    Delete ecommerceproject directory

--- ecommerceproject/ecommerceapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models 
-
-class product(models.Model):
-    name=models.CharField(max_length=15)
-    price=models.DecimalField(max_digits=5,decimal_field=2)
-    description=models.TextField()
-    brand=models.CharField(max_length=25)
-
